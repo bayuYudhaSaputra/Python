30567d96acf8ca2289f4be235662ae397db5809d
bayuYS <bayuyudhasaputraqed@gmail.com>
2024-09-10 01:48:47 +0700

    Tambah file 0110

---
+++ Python_Liang/0110-averageSpeed.py
@@ -0,0 +1,12 @@
+# =============================================================================================================
+# Programming Exercise 01.10
+# Oleh      : #bayuyudhasaputra
+# Sumber    : https://media.pearsoncmg.com/bc/abp/cs-resources/products/product.html#product,isbn=0133050556
+# -------------------------------------------------------------------------------------------------------------
+# 01.10. Average speed
+#   Assume a runner runs 14 kilometers in 45 minutes and 30 seconds. 
+#   Write a program that displays the average speed in miles per hour. 
+#   (Note that 1 mile is 1.6 kilometers.)
+# ==============================================================================================================
+
+print("Speed = ", (14 * 1.6) / ((45/60) + (30/3600)), " miles per hour")

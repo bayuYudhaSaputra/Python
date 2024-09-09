c7f906e21994af30d00d2dc298f8b72cdd92f09b
bayuYS <bayuyudhasaputraqed@gmail.com>
2024-09-09 09:59:10 +0700

    Tambah file 0103

---
+++ Python_Liang/0103-displayPattern.py
@@ -0,0 +1,15 @@
+# =============================================================================================================
+# Programming Exercise 01.03
+# Oleh      : #bayuyudhasaputra
+# Sumber    : https://media.pearsoncmg.com/bc/abp/cs-resources/products/product.html#product,isbn=0133050556
+# -------------------------------------------------------------------------------------------------------------
+# 01.03. Display a pattern
+#   Write a program that displays the following pattern:
+#   https://photos.app.goo.gl/MoQAN3NKKtbS4rYE9
+# ==============================================================================================================
+
+print(" FFFFFFF   U     U   NN    NN ")
+print(" FF        U     U   NNN   NN ")
+print(" FFFFFFF   U     U   NN N  NN ")
+print(" FF         U   U    NN  N NN ")
+print(" FF          UU      NN   NNN ")

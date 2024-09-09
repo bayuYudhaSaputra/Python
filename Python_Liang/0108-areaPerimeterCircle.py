6daf794b4b3cbbed11dfef406bad879d5a081469
bayuYS <bayuyudhasaputraqed@gmail.com>
2024-09-10 00:35:53 +0700

    Tambah file 0108

---
+++ Python_Liang/0108-areaPerimeterCircle.py
@@ -0,0 +1,14 @@
+# =============================================================================================================
+# Programming Exercise 01.08
+# Oleh      : #bayuyudhasaputra
+# Sumber    : https://media.pearsoncmg.com/bc/abp/cs-resources/products/product.html#product,isbn=0133050556
+# -------------------------------------------------------------------------------------------------------------
+# 01.08. Area and perimeter of a circle
+#   Write a program that displays the area and perimeter of a circle that has a radius of 5.5 
+#   using the following formulas:
+#       area = radius *  radius *  Phi
+#       perimeter = 2 *  radius *  Phi
+# ==============================================================================================================
+
+print("area = ", 3.14 * 5.5 * 5.5)
+print("Perimeter = ", 2 * 3.14 * 5.5)

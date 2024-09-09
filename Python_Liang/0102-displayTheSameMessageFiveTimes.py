2b150b77cf6f1b38f9f03d473f5f16f99fc4bcb7
bayuYS <bayuyudhasaputraqed@gmail.com>
2024-09-09 09:41:45 +0700

    tambah file 0102

---
+++ Python_Liang/0102-displayTheSameMessageFiveTimes.py
@@ -0,0 +1,15 @@
+# =============================================================================================================
+# Programming Exercise 01.02
+# Oleh      : #bayuyudhasaputra
+# Sumber    : https://media.pearsoncmg.com/bc/abp/cs-resources/products/product.html#product,isbn=0133050556
+# -------------------------------------------------------------------------------------------------------------
+# 01.02. Display the same message five times
+#   Write a program that displays Welcome to Python five times.
+
+# ==============================================================================================================
+
+print("Welcome to Python")
+print("Welcome to Python")
+print("Welcome to Python")
+print("Welcome to Python")
+print("Welcome to Python")

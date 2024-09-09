1341994fd4acdbedec7b73c476bec57faeea2376
bayuYS <bayuyudhasaputraqed@gmail.com>
2024-09-10 01:13:56 +0700

    Tambah 0109

---
+++ Python_Liang/0109-areaAndPerimeterRectangle.py
@@ -0,0 +1,12 @@
+# =============================================================================================================
+# Programming Exercise 01.09
+# Oleh      : #bayuyudhasaputra
+# Sumber    : https://media.pearsoncmg.com/bc/abp/cs-resources/products/product.html#product,isbn=0133050556
+# -------------------------------------------------------------------------------------------------------------
+# 01.09. Area and perimeter of a rectangle
+#   Write a program that displays the area and perimeter of a rectangle with the width of 4.5 and 
+#   height of 7.9 using the follow- ing formula:
+#       area = width * height
+# ==============================================================================================================
+
+print("Area = ", 4.5 * 7.9)

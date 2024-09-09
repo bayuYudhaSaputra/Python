176c459f42b3e2bac4ac4e98f7588accf7cfe12a
bayuYS <bayuyudhasaputraqed@gmail.com>
2024-09-09 09:34:44 +0700

    Revisi indentasi Message.

--- Python_Liang/0101-displayThreeDifferentMessage.py
+++ Python_Liang/0101-displayThreeDifferentMessage.py
@@ -1,3 +1,14 @@
+# =============================================================================================================
+# Programming Exercise 01.01
+# Oleh      : #bayuyudhasaputra
+# Sumber    : https://media.pearsoncmg.com/bc/abp/cs-resources/products/product.html#product,isbn=0133050556
+# -------------------------------------------------------------------------------------------------------------
+# 1.1 (Display three different messages) 
+#   Write a program that displays Welcome to Python, 
+#   Welcome to Computer Science, and Programming is fun.
+# 
+# ==============================================================================================================
+
 print("Welcome to Python")
 print("Welcome to Computer Science")
 print("Programming is fun")

45cad37c94c4cd5204e27de102a66ff22cbe32a9
bayuYS <bayuyudhasaputraqed@gmail.com>
2024-09-10 13:42:50 +0700

    Tambahkan file 0114

---
+++ Python_Liang/0114-drawTriangle.py
@@ -0,0 +1,21 @@
+# =============================================================================================================
+# Programming Exercise 01.14.
+# Oleh      : #bayuyudhasaputra
+# Sumber    : https://media.pearsoncmg.com/bc/abp/cs-resources/products/product.html#product,isbn=0133050556
+# -------------------------------------------------------------------------------------------------------------
+# 01.14. Turtle: draw a triangle
+#   Write a program that draws a triangle as shown in
+#   
+# ==============================================================================================================
+
+import turtle # import modul turtle
+
+turtle.right(60) # arahkan pen 60 derajat ke kanan
+turtle.forward(100) # buat garis 100px
+turtle.right(120) # arahkan pen 120 ke kanan
+turtle.forward(100) # buat garis 100px
+turtle.right(120) # arahkan pena 120 ke kanan
+turtle.forward(100) # buat garis 100px
+
+
+turtle.done()

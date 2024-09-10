7340aec89f2e06388075f97e27d67f232c4e1f3d
bayuYS <bayuyudhasaputraqed@gmail.com>
2024-09-10 14:07:28 +0700

    Tambahkan file 0115

---
+++ Python_Liang/0115-drawTwoTriangle.py
@@ -0,0 +1,24 @@
+# =============================================================================================================
+# Programming Exercise 01.15.
+# Oleh      : #bayuyudhasaputra
+# Sumber    : https://media.pearsoncmg.com/bc/abp/cs-resources/products/product.html#product,isbn=0133050556
+# -------------------------------------------------------------------------------------------------------------
+# 01.15. Turtle: draw two triangles
+#   Write a program that draws two triangles as shown in
+#   
+# ==============================================================================================================
+
+import turtle # import modul turtle
+
+turtle.right(60) # arahkan 60 derajat ke kanan
+turtle.forward(100) # buat garis 100px
+turtle.right(120) # arahkan 120 derajat ke kanan
+turtle.forward(100) # buat garis 100px
+turtle.right(120) # arahkan 120 derajat ke kanan
+turtle.forward(200) # buat garis 200px
+turtle.left(120) # arahkan 120 derajat ke kiri
+turtle.forward(100) # buat garis 100px
+turtle.left(120) # arahkan 120 derajat ke kiri
+turtle.forward(100) # buat garis 100px
+
+turtle.done()

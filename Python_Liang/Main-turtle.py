cba9748b0f0aed3f76b03b319dbf280276008bc6
bayuYS <bayuyudhasaputraqed@gmail.com>
2024-09-10 08:12:40 +0700

    Tambah file template turtle "Main-turtle"

---
+++ Python_Liang/Main-turtle.py
@@ -0,0 +1,13 @@
+# =============================================================================================================
+# Programming Exercise xx.xx
+# Oleh      : #bayuyudhasaputra
+# Sumber    : https://media.pearsoncmg.com/bc/abp/cs-resources/products/product.html#product,isbn=0133050556
+# -------------------------------------------------------------------------------------------------------------
+# xx.xx. Judul Exercise
+#   (Tuliskan soal di sini)
+# ==============================================================================================================
+
+import turtle # import modul turtle
+
+
+turtle.done()

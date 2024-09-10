57a667ddc6d3079b5307f9e3fc49ecb1e17f9f94
bayuYS <bayuyudhasaputraqed@gmail.com>
2024-09-10 08:30:20 +0700

    Tambah file coba Turtle

---
+++ Python_Liang/contohTurtle.py
@@ -0,0 +1,26 @@
+# =============================================================================================================
+# Programming Exercise xx.xx
+# Oleh      : #bayuyudhasaputra
+# Sumber    : https://media.pearsoncmg.com/bc/abp/cs-resources/products/product.html#product,isbn=0133050556
+# -------------------------------------------------------------------------------------------------------------
+# xx.xx. Judul Exercise
+#   (Tuliskan soal di sini)
+# ==============================================================================================================
+
+import turtle # import modul turtle
+
+turtle.showturtle() 
+turtle.write("Welcome to Python") # tampilkan teks
+turtle.forward(100) # garis maju sesuai arah panah sepanjang 100px
+turtle.right(90) # memutar arah panah 90 derajat searah jarum jam
+turtle.color("red") # mengubah warna garis menjadi merah
+turtle.forward(50) # garis maju sesuai arah panah sejauh 50px
+turtle.right(90) # memutar arah panah 90 derajat searah jarum jam
+turtle.color("green") # ubah warna hijau
+turtle.forward(100) # buat garis sesuai arah panah sepanjang 100px
+turtle.right(45) # memutar arah panah 45 derajat searah jarum jam
+turtle.forward(80) # buat garis sesuai arah panah sepanjang 80px
+
+
+
+turtle.done() # Menghentikan turtle 

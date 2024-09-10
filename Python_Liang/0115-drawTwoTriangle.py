3444a837972ae494eea8264a5d76fef2f022cdea
bayuYS <bayuyudhasaputraqed@gmail.com>
2024-09-10 14:10:11 +0700

    revisi format komentar

--- Python_Liang/0115-drawTwoTriangle.py
+++ Python_Liang/0115-drawTwoTriangle.py
@@ -10,15 +10,15 @@
 
 import turtle # import modul turtle
 
-turtle.right(60) # arahkan 60 derajat ke kanan
+turtle.right(60)    # arahkan 60 derajat ke kanan
 turtle.forward(100) # buat garis 100px
-turtle.right(120) # arahkan 120 derajat ke kanan
+turtle.right(120)   # arahkan 120 derajat ke kanan
 turtle.forward(100) # buat garis 100px
-turtle.right(120) # arahkan 120 derajat ke kanan
+turtle.right(120)   # arahkan 120 derajat ke kanan
 turtle.forward(200) # buat garis 200px
-turtle.left(120) # arahkan 120 derajat ke kiri
+turtle.left(120)    # arahkan 120 derajat ke kiri
 turtle.forward(100) # buat garis 100px
-turtle.left(120) # arahkan 120 derajat ke kiri
+turtle.left(120)    # arahkan 120 derajat ke kiri
 turtle.forward(100) # buat garis 100px
 
 turtle.done()

776d9320e40279789cf8cfd6913abfdbe63c3959
bayuYS <bayuyudhasaputraqed@gmail.com>
2024-11-22 17:51:05 +0700

    Tambah file 0204

---
+++ Python_Liang/0204-KonversiPoundKeKilogram.py
@@ -0,0 +1,17 @@
+# ====================================================
+# 0204. Konversi Pound Ke Kg
+# Oleh : #bayuyudhasaputra
+# ----------------------------------------------------
+# Tuliskan program yang meminta input berat dalam pound
+# kemudian dikonversi ke satuan kilogram jika 
+# 1 pound = 0.454 Kg
+# ====================================================
+
+# Input berat dalam pound
+pound = eval(input("Input berat dalam satuan pound (Misal 55.5): "))
+
+# Konversi berat dari pound ke kilogram
+kilogram = 0.454 * pound
+
+# Tampilkan hasil konversi
+print("Berat", pound, "pound sama dengan", kilogram, "Kg.")

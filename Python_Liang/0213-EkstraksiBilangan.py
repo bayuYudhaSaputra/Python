1a622f439377b4f78e082c982b1af60590b27f63
bayuYS <bayuyudhasaputraqed@gmail.com>
2025-02-11 09:39:54 +0700

    Tambah file 0213

--- Python_Liang/0213-EkstraksiBilangan.py
+++ Python_Liang/0213-EkstraksiBilangan.py
@@ -10,3 +10,35 @@ terbalik.
 ====================================================
 '''
 
+# 1. Meminta input bilangan ribuan
+bilangan = eval(input("Input bilangan 4 digit (Mis. 3125) : "))
+
+# Ekstrak angka ribuan
+ribuan = bilangan // 1000
+
+# Ambil 3 digit terakhir
+sisaRibuan = bilangan % 1000
+
+# Ekstrak angka ratusan
+ratusan = sisaRibuan // 100
+
+# Ambil 2 digit terakhir
+sisaRatusan = sisaRibuan % 100
+
+# Ekstrak angka puluhan
+puluhan = sisaRatusan // 10
+
+# Ekstrak angka satuan
+satuan = sisaRatusan % 10
+
+# Konversi ke string
+ribuan = str(ribuan)
+ratusan = str(ratusan)
+puluhan = str(puluhan)
+satuan = str(satuan)
+
+# Urutkan terbalik
+bilanganTerbalik = satuan + puluhan + ratusan + ribuan
+
+print("Urutan terbalik dari bilangan", bilangan, "adalah", bilanganTerbalik)
+

13d3b9d6b18f4569cf7caf98d12d4dc339ec6623
bayuYS <bayuyudhasaputraqed@gmail.com>
2025-02-11 09:39:31 +0700

    Tambah file 0213

---
+++ Python_Liang/0213-EkstraksiBilangan.py
@@ -0,0 +1,12 @@
+'''
+====================================================
+0213. Mengurutkan Terbalik Bilangan Ribuan
+Oleh : #bayuyudhasaputra
+----------------------------------------------------
+Tuliskan program yang meminta input kepada pengguna 
+untuk menginput bilangan 4 digit. Kemudian, 
+program menampilkan angka-angka dengan urutan 
+terbalik.
+====================================================
+'''
+

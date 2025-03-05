f58f21637f892f92ea393670c8aa0242b4de5038
bayuYS <bayuyudhasaputraqed@gmail.com>
2025-03-05 08:29:53 +0700

    Tambah file 0219

---
+++ Python_Liang/0219-hitungInvestasi.py
@@ -0,0 +1,23 @@
+'''
+====================================================
+0219. Hitung Investasi
+Oleh : #bayuyudhasaputra
+----------------------------------------------------
+Tuliskan program yang menampilkan nilai akhir 
+investasi jika pengguna menginput nilai nilai 
+investasi awal, bunga tahunan dan jangka waktu 
+investasi dalam satuan tahun.
+====================================================
+
+'''
+
+# 1. Input nilai investasi awal, persentase bunga tahunan dan jangka waktu
+nilaiAwal = eval(input("Input nilai awal investasi : Rp"))
+bunga = eval(input("Input persentase bunga tahunan : "))
+tahun = eval(input("Input jangka waktu investasi (dalam satuan tahun) : "))
+
+# 2. Menentukan nilai akhir investasi.
+nilaiAkhir = nilaiAwal * (1 + (bunga / 100)) ** tahun
+
+# 3. Tampilkan hasil perhitungan nilai akhir investasi.
+print("Nilai akhir investasi adalah Rp", nilaiAkhir)

77d389fc46c380ab8e49affe91f511eaac5ea1f4
bayuyudhasaputraqed@gmail.com <bayuyudhasaputraqed@gmail.com>
2025-04-04 21:18:27 +0700

    Tambah file 0305

---
+++ Python_Liang/0305-LuasSegiNBeraturan.py
@@ -0,0 +1,29 @@
+'''
+   ====================================
+   03.05. Luas Segi-n Beraturan
+   Oleh : #bayuyudhasaputra
+   ------------------------------------
+   Buatlah program yang menerima input
+   segi apa dan panjang sisi segi-n 
+   tersebut. Program menentukan luas
+   segi-n tersebut.
+   ====================================
+'''
+
+import math
+
+# 1. Input segi-n apa yang dimaksud.
+n = eval(input("Input segi apa yang dimaksud: "))
+
+# 2. Input panjang sisi segi-n 
+sisi = eval(input("Input panjang sisi segi-n: "))
+
+# 3. Hitung luas segi-n
+luas = (n * sisi ** 2) / (4 * math.tan(math.pi / n))
+
+# 4. Tampilkan luas
+print("Luas segi-", n,
+      "dengan panjang sisi", sisi,
+      "adalah", luas, "."
+      )
+

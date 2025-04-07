7d60b88945f1bf30f52c0e0b21a1aac3bcebe288
bayu <bayuyudhasaputraqed@gmail.com>
2025-04-06 23:57:01 -0400

    Tambah file 0307

--- Python_Liang/0307-ASCIIRandom.py
+++ Python_Liang/0307-ASCIIRandom.py
@@ -12,7 +12,14 @@
 import time
 import random
 
-# 1. Buat seed (pengubah waktu random)
+# 1. Jalankan seed (pengubah waktu random)
 seed = int(time.time())
 
-print(seed)
+# 2. Buat kode ASCII
+kode_ascii = seed % 127
+
+# 3. Konversi kode ASCII ke dalam karakter
+karakter = chr(kode_ascii)
+
+# 4. Tampilkan karakter random
+print("Hasil karakter random adalah", karakter)

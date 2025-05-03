ca08786d9d58d391db8726682010a51e6eb071a9
bayuYudhaSaputra <bayuyudhasaputraqed@gmail.com>
2025-05-03 15:50:36 -0400

    modif luas

--- Python_Liang/0301-LuasSegilima.py
+++ Python_Liang/0301-LuasSegilima.py
@@ -1,21 +1,27 @@
 '''
-    ======================================
+    ======================================================================
     03.01. Hitung Luas Segitima Beraturan
     Oleh: #bayuyudhasaputra
-    --------------------------------------
-    Buatlah program menentukan luas segilima dimana pengguna menginput jarak titik pusat segilima dengan titik sudut dari segilima tersebut.
-    ======================================
+    ----------------------------------------------------------------------
+    Tuliskan program yang menampilkan luas segilima serta luasnya dimana
+    pengguna menginput jarak antara titik sudut dengan titik pusat
+    segilima
+    ======================================================================
 '''
+import turtle
 import math
 
-# 1. Input jarak titik pusat ke titik sudut
-jarak_titik_pusat_sudut = eval(input("Input jarak titik pusat ke titik sudut segilima: "))
+# 1. Input jarak titik pusat ke titik sudut (r)
+r = eval(input("Input jarak titik pusat ke titik sudut segilima: "))
+PI = math.pi
 
-# 2. Hitung panjang sisi
-panjang_sisi = 2 * jarak_titik_pusat_sudut * math.sin(math.pi / 5)
+# 2. Hitung luas segilima
+luas = ((5 * r ** 2) / 2) * math.sin((2 * PI) / 5)
 
-# 3. Hitung luas segilima
-luas = (3 * math.sqrt(3) / 2) * pow(panjang_sisi, 2)
+# 3. Gambar segilima
+turtle.color("blue")
+turtle.penup()
+
 
 # 4 Tampilkan luas segilima
 print("Luas segilima adalah ", luas)

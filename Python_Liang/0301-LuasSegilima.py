dfe7bcafacf81a5a185fc9495571db7963667852
bayuYudhaSaputra <bayuyudhasaputraqed@gmail.com>
2025-05-05 07:42:27 -0400

    gambar segilima di turtle

--- Python_Liang/0301-LuasSegilima.py
+++ Python_Liang/0301-LuasSegilima.py
@@ -17,11 +17,25 @@ PI = math.pi
 
 # 2. Hitung luas segilima
 luas = ((5 * r ** 2) / 2) * math.sin((2 * PI) / 5)
+luas = format(luas, ".2f") #dibulatkan 2 angka desimal
+output_luas = "Luas segilima adalah " + str(luas)
 
 # 3. Gambar segilima
-turtle.color("blue")
+# 3a. Membuat segilima
 turtle.penup()
+turtle.goto(0, -r)
+turtle.pendown()
+turtle.color("blue") # sisi berwarna biru
+turtle.circle(r, steps = 5) # membuat segilima beraturan
+
+# 3b. Menulis pesan
+turtle.penup()
+turtle.goto(-r, (-r - 50))
+turtle.write(output_luas)
+turtle.hideturtle() # turtle disembunyikan
+
+turtle.done() # layar turtle berhenti.
 
 
 # 4 Tampilkan luas segilima
-print("Luas segilima adalah ", luas)
+print(output_luas)

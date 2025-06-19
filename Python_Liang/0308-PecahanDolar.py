b5f9bb6f2b7a2300f3e6a10eae34ff1ed5961b3a
bayuYS <bayuyudhasaputraqed@gmail.com>
2025-06-19 10:39:33 +0700

    ROmbak file 0308

--- Python_Liang/0308-PecahanDolar.py
+++ Python_Liang/0308-PecahanDolar.py
@@ -1,45 +1,60 @@
 '''
     ===========================================
-    3.7. Hitung Pecahan Dolar
+    3.8. Hitung Pecahan Dolar
     Oleh : #bayuyudhasaputra
     -------------------------------------------
-    Buatlah program yang menerima input
-    sejumlah uang dalam satuan dolar. Program
-    mengonversinya ke dalam:
-    - quarter
-    - dime
-    - penny
-    - cent
+    Buatlah program yang menerima input sejumlah 
+    uang dalam satuan dolar Amerika Serikat (USD). 
+    Program mengonversi nominal uang ini ke dalam
+    beberapa:
+        lembar uang 1 dolaran;
+        koin quarter;
+        koin dime;
+        koin nickel;
+        koin penny.
     ===========================================
 '''
 
-# 1. Input nominal uang dalam dolar
-dolar = eval(input("Input nominal uang dalam dolar : "))
-dolar = dolar * 100
+# 1. Input nominal uang dalam USD
+jumlah = eval(input("Input sejumlah uang dalam USD : USD."))
 
-# 2. Hitung banyak quarter
-quarter = int(dolar // 25)
-sisa = dolar % 25
+# 2. Konversi uang dalam USD ini ke dalam cent
+cent = int(jumlah * 100)
 
-# 3. Hitung banyak dime
-dime = int(sisa // 10)
+# 3. Konversi cent ke dalam beberapa lembar 1 dolaran
+satu_dolaran = cent // 100
+
+# 4. Tentukan sisa pembagian
+sisa = cent % 100
+
+# 5. Konversi ke dalam beberapa quarter
+quarter = sisa // 25
+
+# 6. Perbarui sisa
+sisa = sisa % 25
+
+# 7. Konversi ke dalam beberapa dime
+dime = sisa // 10
+
+# 7. Perbarui sisa
 sisa = sisa % 10
 
-# 4. Hitung banyak nickel
-nickel = int(sisa // 5)
+# 8. Konversi ke dalam beberapa nickel
+nickel = sisa // 5
+
+# 9. Perbarui sisa
 sisa = sisa % 5
 
-# 5. Hitung banyak sen
-sen = int(sisa)
+# 10. Konversi ke penny
+penny = sisa
 
-# 6. Kembalikan dolar
-dolar = dolar / 100
-
-# 7. Tampilkan hasil
-print("Uang sejumlah USD.", dolar, "sama dengan \n",
-      "\t", quarter, "quarter; \n",
-      "\t", dime, "dime; \n",
-      "\t", nickel, "nickel; \n",
-      "\t", sen, "cent."
+# Tampilkan hasil konversi
+print("Uang sebesar USD", jumlah, "dapat dinyatakan dalam:", "\n",
+      "\t", satu_dolaran, "lembar uang 1 dolaran;", "\n",
+      "\t", quarter, "koin uang quarter;", "\n",
+      "\t", dime, "koin uang dime;", "\n",
+      "\t", nickel, "koin uang nickel;", "\n",
+      "\t", penny, "koin uang penny.", "\n"
       )
+
 

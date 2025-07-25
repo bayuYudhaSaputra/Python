64e344c69398c1eda6ac5e3f793a8e16b6af84f7
bayuYS <bayuyudhasaputraqed@gmail.com>
2025-07-25 14:32:55 +0700

    0315: Buat file 0315

---
+++ Python_Liang/0315-Senyum.py
@@ -0,0 +1,52 @@
+'''
+=============================================================================================================
+Programming Exercise 03.15
+Oleh      : #bayuyudhasaputra
+Sumber    : https://www.amazon.com/Introduction-Programming-Using-Python-Daniel/dp/0132747189
+-------------------------------------------------------------------------------------------------------------
+03.15. Emoji Senyum
+    Buatlah program untuk menggambar emoji senyun
+==============================================================================================================
+'''
+
+import turtle # import modul turtle
+
+turtle.pensize(5)
+
+# 1. Buat kepala
+turtle.penup()
+turtle.goto(0,-100)
+turtle.pendown()
+turtle.begin_fill()
+turtle.circle(100)
+turtle.fillcolor("yellow")
+turtle.end_fill()
+
+# 2. Buat mata ke-1
+turtle.penup()
+turtle.goto(-30,20)
+turtle.pendown()
+turtle.begin_fill()
+turtle.circle(10)
+turtle.fillcolor("black")
+turtle.end_fill()
+
+# 3. Buat mata ke-2
+turtle.penup()
+turtle.goto(30,20)
+turtle.pendown()
+turtle.begin_fill()
+turtle.circle(10)
+turtle.fillcolor("black")
+turtle.end_fill()
+
+# 4. Buat mulut
+turtle.penup()
+turtle.goto(-45, -45)
+turtle.pendown()
+turtle.goto(0,-70)
+turtle.goto(45,-45)
+
+
+turtle.hideturtle()
+turtle.done()

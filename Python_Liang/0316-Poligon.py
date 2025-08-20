3f8217019bd184330f4a988773b656129c150799
bayuYS <bayuyudhasaputraqed@gmail.com>
2025-08-20 13:00:45 +0700

    0316 : memperbaiki panjang sisi  setiap poligon

--- Python_Liang/0316-Poligon.py
+++ Python_Liang/0316-Poligon.py
@@ -1,13 +1,13 @@
 '''
-=============================================================================================================
+=================================================================================================
 Programming Exercise 03.16
 Oleh      : #bayuyudhasaputra
 Sumber    : https://www.amazon.com/Introduction-Programming-Using-Python-Daniel/dp/0132747189
--------------------------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------------------
 03.16. Membuat Poligon
    Buatlah segitiga, segiempat, segilima, segienam dan segidelapan beraturan dengan
    sisi bawah sejajar sumbu-x. Poligon-poligon ini diberi warna yang berbeda.
-==============================================================================================================
+=================================================================================================
 '''
 
 import turtle # import modul turtle
@@ -31,7 +31,7 @@ turtle.end_fill()
 turtle.left(120) # kursor dikembalikan ke arah kanan
 
 # 2. Buat persegi
-sisiSegi4 = sisiSegi3 * math.cos(math.pi / 6) # pi / 6 = 60 derajat
+sisiSegi4 = sisiSegi3 * math.sin((60 / 180) * math.pi) # (60 / 180) * math.pi = 60 derajat
 turtle.penup()
 turtle.goto(-275, -100)
 turtle.pendown()
@@ -48,63 +48,66 @@ turtle.end_fill()
 turtle.left(90) # Kursor dikembalikan ke arah kanan
 
 # 3. Buat segilima beraturan
+sisiSegi5 = (2 * sisiSegi3 * math.sin((60 / 180) * math.pi) * math.tan((36 / 180) * math.pi)) / (2 * math.tan((36 / 180) * math.pi) + 1)
 turtle.penup()
 turtle.goto(-100, -100)
 turtle.pendown()
 turtle.begin_fill()
-turtle.forward(100)
+turtle.forward(sisiSegi5)
 turtle.left(72)
-turtle.forward(100)
+turtle.forward(sisiSegi5)
 turtle.left(72)
-turtle.forward(100)
+turtle.forward(sisiSegi5)
 turtle.left(72)
-turtle.forward(100)
+turtle.forward(sisiSegi5)
 turtle.left(72)
-turtle.forward(100)
+turtle.forward(sisiSegi5)
 turtle.fillcolor("yellow")
 turtle.end_fill()
 turtle.left(72) # Kursor dikembalikan ke arah kanan
 
 # 4. Buat segienam beraturan
+sisiSegi6 = (1 / 2) * sisiSegi3
 turtle.penup()
 turtle.goto(100, -100)
 turtle.pendown()
 turtle.begin_fill()
-turtle.forward(100)
+turtle.forward(sisiSegi6)
 turtle.left(60)
-turtle.forward(100)
+turtle.forward(sisiSegi6)
 turtle.left(60)
-turtle.forward(100)
+turtle.forward(sisiSegi6)
 turtle.left(60)
-turtle.forward(100)
+turtle.forward(sisiSegi6)
 turtle.left(60)
-turtle.forward(100)
+turtle.forward(sisiSegi6)
 turtle.left(60)
-turtle.forward(100)
+turtle.forward(sisiSegi6)
 turtle.fillcolor("green")
 turtle.end_fill()
 turtle.left(60) # Kursor dikembalikan ke arah kanan
 
 # 5. Buat segi delapan beraturan
+sisiSegi8 = sisiSegi3 * math.sin((60 / 180) * math.pi) * math.tan((22.5 / 180) * math.pi)
 turtle.penup()
 turtle.goto(400, -100)
 turtle.pendown()
 turtle.begin_fill()
-turtle.forward(100)
+turtle.forward(sisiSegi8)
 turtle.left(45)
-turtle.forward(100)
+turtle.forward(sisiSegi8)
 turtle.left(45)
-turtle.forward(100)
+turtle.forward(sisiSegi8)
 turtle.left(45)
-turtle.forward(100)
+turtle.forward(sisiSegi8)
 turtle.left(45)
-turtle.forward(100)
+turtle.forward(sisiSegi8)
 turtle.left(45)
-turtle.forward(100)
+turtle.forward(sisiSegi8)
 turtle.left(45)
-turtle.forward(100)
+turtle.forward(sisiSegi8)
 turtle.left(45)
-turtle.forward(100)
+turtle.forward(sisiSegi8)
 turtle.fillcolor("blue")
 turtle.end_fill()
 turtle.left(45)
